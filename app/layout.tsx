6d8b6c84aa4e988cfbf9277c92c44af945585357
JorgeFPeres <jorge-peres@live.com>
2024-11-14 21:11:03 -0300

    feat: react query configs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { SidebarProvider } from '@/components/ui/sidebar'
 import { AppSidebar } from '@/components/app-sidebar'
+import { ReactQueryClientProvider } from '@/components/react-query-client-providers'
 
 export default function RootLayout({
   children,
@@ -8,15 +9,17 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang='en'>
-      <body className='antialiased' suppressHydrationWarning>
-        <SidebarProvider>
-          <AppSidebar />
-          <main className='flex flex-1 flex-col gap-4 p-4 pt-0 w-full h-screen'>
-            {children}
-          </main>
-        </SidebarProvider>
-      </body>
-    </html>
+    <ReactQueryClientProvider>
+      <html lang='en'>
+        <body className='antialiased' suppressHydrationWarning>
+          <SidebarProvider>
+            <AppSidebar />
+            <main className='flex flex-1 flex-col gap-4 p-4 pt-0 w-full h-screen'>
+              {children}
+            </main>
+          </SidebarProvider>
+        </body>
+      </html>
+    </ReactQueryClientProvider>
   )
 }
